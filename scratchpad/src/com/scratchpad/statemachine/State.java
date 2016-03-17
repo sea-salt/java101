03129bc1e9574391f1c22986d02043a817aa3435
Larissa.Friedel <larissa.friedel@gmx.de>
2016-03-17 08:19:40 +0000

    Adjusted OPEN > start requirement

---
+++ scratchpad/src/com/scratchpad/statemachine/State.java
@@ -0,0 +1,11 @@
+package com.scratchpad.statemachine;
+
+/**
+ * Created by larissa on 13.03.16.
+ */
+public enum State {
+    OPEN,
+    IN_PROGRESS,
+    AWAITING_APPROVAL,
+    FINISHED
+}
--- scratchpad/src/com/scratchpad/statemachine/TaskStateMachine.java
+++ scratchpad/src/com/scratchpad/statemachine/TaskStateMachine.java
@@ -21,19 +21,45 @@ import java.util.Scanner;
  * -> means 'results in'
  * You figure out what data type State should be!
  */
+
 public class TaskStateMachine {
     private Scanner scanner;
-    
+
     public TaskStateMachine() {
         scanner = new Scanner(System.in);
     }
-    
+
     public void run() {
-        // TODO: Implement state machine here!
+        State state = null;
+        while (true) {
+            printCurrentState(state);
+            System.out.println("Please enter a command:");
+            switch(getUserInput()) {
+                case "start":
+                    if (State.OPEN.equals(state)) {
+                        state = State.IN_PROGRESS;
+                    }
+                    else {
+                        System.out.println("This command can only be used in OPEN state");
+                    }
+                    break;
+                case "complete":
+                    state = State.AWAITING_APPROVAL;
+                    break;
+                case "accept":
+                    state = State.FINISHED;
+                    break;
+                case "reject":
+                    state = State.IN_PROGRESS;
+                    break;
+                default:
+                    System.out.println("This is invalid input. Please try again.");
+            }
+        }
     }
     
-    private void printCurrentState() {
-        System.out.println("Current state: ");
+    private void printCurrentState(State state) {
+        System.out.println("Current state: " + state);
     }
     
     private String getUserInput() {
